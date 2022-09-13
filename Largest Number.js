a68eda45db5734037453e2049ed95280cb8484f5
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-13 13:33:01 +0300

    committed

---
+++ Largest Number.js
@@ -0,0 +1,4 @@
+function solve(Num1, Num2, Num3) {
+    let maxNum = Math.max(Num1, Num2, Num3)
+    console.log(`The largest number is ${maxNum}.`);
+}
---
+++ String Length.js
@@ -0,0 +1,8 @@
+function solve(word1, word2, word3) {
+    let sum = word1.length + word2.length + word3.length
+    console.log(sum);
+
+        console.log(Math.floor(sum / 3))
+        }
+       
+solve('chocolate', 'ice cream', 'cake')
