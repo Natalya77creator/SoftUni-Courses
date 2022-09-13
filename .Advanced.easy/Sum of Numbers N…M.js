6b37aba9841ec6907aa7ccfcb5b4130b0958dab5
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-13 15:00:06 +0300

    commit

---
+++ .Advanced.easy/Sum of Numbers N…M.js
@@ -0,0 +1,10 @@
+function solve(str1, str2) {
+    let num1 = Number(str1);
+    let num2 = Number(str2);
+    let sum = 0
+    for (i = num1; i <= num2; i++) {
+        sum += i
+    }
+    console.log(sum);
+    }
+solve('-8', '20')
