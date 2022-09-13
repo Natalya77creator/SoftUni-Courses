c25b70c1209702e4c2c5480b28f14276c0772cc8
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-13 16:29:38 +0300

    Create Aggregate Elements.js

---
+++ .Advanced.easy/Aggregate Elements.js
@@ -0,0 +1,17 @@
+function solve(params) {
+    let sum = 0;
+    let sumOne = 0;
+    let concat = '';
+    for (let index = 0; index < params.length; index++) {
+        let element = params[index];
+        sum += element;
+        sumOne += 1 / element;
+        element = element.toString();
+        concat += element
+      
+    }
+    console.log(sum);
+    console.log(sumOne);
+    console.log(concat);
+}
+solve([1, 2, 3])
