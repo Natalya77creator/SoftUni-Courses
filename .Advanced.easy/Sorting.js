2dd65c504c9feafc8ad58921d576107cc9db0558
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-17 11:11:49 +0300

    Update Sorting.js

--- .Advanced.easy/Sorting.js
+++ .Advanced.easy/Sorting.js
@@ -1,11 +1,10 @@
-function solve(input) {
-    let sort = input.sort((a, b) => a - b)
-    let bigNumbers = (sort.splice(sort.length / 2, sort.length - 1)).reverse()
-    let arr = []
- 
-    for (let i = 0; i < sort.length; i++) {
-        arr.push(sort[i], bigNumbers[i])
+function solve(input){
+    let result = [];
+    array = input.sort((a, b) => a - b);
+    while(array.length !== 0){
+      result.push(array[0]) && array.shift();
+      result.push(array[array.length-1]) && array.pop();
     }
-    console.log(arr)
-}
+ return result
+ }
 solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56, 99])
