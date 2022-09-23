cc252a2d083eb5f9a4b1f94b7fdb45ba1e6e705f
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-23 09:12:32 +0300

    Create Show More.js

---
+++ .DOM easy/Show More.js
@@ -0,0 +1,6 @@
+function showText() {
+    let textText = document.getElementById('text')
+    textText.style.display = 'inline';
+    let moreText = document.getElementById('more');
+    moreText.style.display = 'none';
+}
