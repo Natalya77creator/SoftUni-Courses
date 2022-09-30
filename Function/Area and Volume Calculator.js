5ebc5122f9e06d6a700f5515d43938f330114060
Natalya77 <natalia.pencheva77@yahoo.com>
2022-09-30 12:41:45 +0300

    Update Area and Volume Calculator.js

--- Function/Area and Volume Calculator.js
+++ Function/Area and Volume Calculator.js
@@ -1,14 +1,27 @@
 function solve(area, vol, input) {
-    let obj = JSON.parse(input);
-    function cal(ojec) {
-        let areaObj = Math.abs(area.call(ojec));
-        let volObj = Math.abs(vol.call(ojec));
-        return {arae : areaObj, volume : volObj}
+    let data = JSON.parse(input);
+    let result = [];
+
+    for (const entry of data) {
+        let calArea = area.call(entry);
+        let calVol = vol.call(entry);
+        result.push({
+            area: calArea,
+            volume: calVol,
+        });
     }
-    return obj.map(cal)
+    return result
 }
-solve(area, vol,`[
+let area = function area() {
+    return Math.abs(this.x * this.y);
+};
+let vol = function vol() {
+  return Math.abs(this.x * this.y * this.z);
+};
+
+let result = solve(area, vol,`[
     {"x":"1","y":"2","z":"10"},
     {"x":"7","y":"7","z":"10"},
     {"x":"5","y":"2","z":"10"}
     ]`)
+console.log(JSON.stringify(result));
