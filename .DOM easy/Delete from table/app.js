8769664382fc695af9aff19355ccb37097a39044
Natalya77 <natalia.pencheva77@yahoo.com>
2022-10-04 09:28:01 +0300

    Update app.js

--- .DOM easy/Delete from table/app.js
+++ .DOM easy/Delete from table/app.js
@@ -1,18 +1,25 @@
 function deleteByEmail() {
-    let target = document.getElementsByName("email")[0].value;
-    let emails = document.querySelectorAll("#customers tr td:last-child");
-    let deleted = false;
-    for (let email of emails) {
-        if (email.textContent === target) {
-            let row = email.parentNode;
-            row.parentNode.removeChild(row);
-            deleted = true;
-            break;
-        }
-    }
-    if (deleted) {
-        document.getElementById("result").textContent = "Deleted."
-    } else {
-        document.getElementById("result").textContent = "Not found."
-    }
+  let entr = document.querySelector('input[type="text"]').value;
+  let bnt = document.getElementsByTagName("button")[0];
+
+  bnt.addEventListener("click", deleteItem);
+  let isTrue = false
+  function deleteItem(e) {
+    let cldOnen = Array.from(document.getElementsByTagName("tbody")[0].children)
+   for (const child of cldOnen) {
+     for (const ch of Array.from(child.children)) {
+      if (ch.innerText === entr) {
+        let row = ch.parentNode;
+        row.parentNode.removeChild(row);
+        isTrue = true;
+        debugger
+     }
+   }
+  }
+      if (isTrue === true) {
+        document.getElementById('result').textContent = 'Deleted'
+      } else {
+        document.getElementById('result').textContent = 'Not found'
+      }
+  }
 }
