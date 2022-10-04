1d4722f3168253bb260ce2abdd12e70fa1a2cbc2
Natalya77 <natalia.pencheva77@yahoo.com>
2022-10-04 09:28:35 +0300

    Update app.js

--- .DOM easy/Mouse-Gradient/app.js
+++ .DOM easy/Mouse-Gradient/app.js
@@ -4,10 +4,12 @@
         gradient.addEventListener('mouseout', gradientOut);
         function gradientMove(event) {
             let power = event.offsetX / (event.target.clientWidth - 1);
+            debugger
             power = Math.trunc(power * 100);
             document.getElementById('result').textContent = power + "%";
         }
         function gradientOut(event) {
             document.getElementById('result').textContent = "";
+            
         }
     };
